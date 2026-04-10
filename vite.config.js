3bea63045045ca8776fc661c81a0db54d12273b8
Ronnie Atok <125497908+Ronnieatok@users.noreply.github.com>
2026-04-10 12:00:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-  }
+  base: './',  // ← THIS is the fix
 })
